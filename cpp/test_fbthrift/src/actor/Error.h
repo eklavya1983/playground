d7967dc6b082158b2656eec797b5b33bac71b396
eklavya1983 <eklavya1983@github.com>
2016-09-18 11:10:08 -0700

    moving around stuff

--- cpp/test_fbthrift/src/actor/Error.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <actor/gen-cpp2/Service_types.h>
-namespace actor {
-using namespace cpp2;
-inline bool ok(const Error &e) {
-    return e == Error::ERR_OK;
-}
-}  // namespace actor
