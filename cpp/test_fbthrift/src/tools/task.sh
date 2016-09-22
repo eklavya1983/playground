b61936b403cd32baff981582782e5ff12e6b3781
eklavya1983 <eklavya1983@github.com>
2016-09-22 05:32:26 -0700

    Task script to automate common dev tasks

---
+++ cpp/test_fbthrift/src/tools/task.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Figure out src dir and tools dir
+SOURCE="${BASH_SOURCE[0]}"
+# resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+TOOLSDIR="$(cd -P "$( dirname "${SOURCE}" )" && pwd )"
+SOURCEDIR="$( cd ${TOOLSDIR}/.. && pwd)"
+printf "srcdir: $SOURCEDIR\n"
+printf "toolsdir: $TOOLSDIR\n"
+
+# includes
+source ${TOOLSDIR}/utils.sh
+
+
+# Compile source code and install artifacts in src/../build folder
+function compile {
+    cd ${SOURCEDIR}/../build
+    cmake -DCMAKE_INSTALL_PREFIX="." ..
+    make && make install
+}
+
+# clean out the build artifacts
+function clean {
+    set -x
+    rm -rf ${SOURCEDIR}/../build/*
+    set +x
+}
+
+# buid source and install artifacts to /op/datom
+function install {
+    cd ${SOURCEDIR}/../build
+    cmake -DCMAKE_INSTALL_PREFIX="/opt/datom" ..
+    make && make install
+}
+
+# call arguments verbatim:
+$@
